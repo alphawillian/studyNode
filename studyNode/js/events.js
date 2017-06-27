b4610ead0121b357ccccd7167470ac8b67c21188
alphawillian <1099763365@qq.com>
2017-06-27 10:50:51 +0800

    about events

--- studyNode/js/events.js
+++ studyNode/js/events.js
@@ -8,17 +8,22 @@ var connectHandler = function connected() {
    console.log('连接成功。');
   
    // 触发 data_received 事件 
-   eventEmitter.emit('data_received');
+   eventEmitter.emit('data_received11');
+   eventEmitter.emit("william");
 }
 
 // 绑定 connection 事件处理程序
 eventEmitter.on('connection', connectHandler);
  
 // 使用匿名函数绑定 data_received 事件
-eventEmitter.on('data_received', function(){
+eventEmitter.on('data_received11', function(){
    console.log('数据接收成功。');
 });
-
+//绑定 William 事件
+eventEmitter.on('william',function(){
+	console.log('自定义William事件触发成功');
+})
+console.log("程序执行完毕1。"); 
 // 触发 connection 事件 
 eventEmitter.emit('connection');
 
--- studyNode/js/events2.js
+++ studyNode/js/events2.js
@@ -1,8 +1,9 @@
-var EventEmitter = require("events").eventEmitter;
-var event = new EventEmitter();
+var events = require("events");
+var event = new events.EventEmitter();
 event.on('some_event',function(){
 	console.log('some_event 事件触发');
 });
 setTimeout(function(){
 	event.emit('some_event');
 },1000);
+console.log("immediately console.log");
