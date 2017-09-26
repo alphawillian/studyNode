0f07c06278d14fe214f4c907f530ef9c4b0f3cc5
zhangguoxin1 <zhangguoxin1@360buyad.local>
2017-09-26 14:40:17 +0800

    111

--- studyNode/js/events.js
+++ studyNode/js/events.js
@@ -5,16 +5,16 @@ var eventEmitter = new events.EventEmitter();
 
 // 创建事件处理程序
 var connectHandler = function connected() {
-   console.log('连接成功。');
-  
-   // 触发 data_received 事件 
+   console.log('连接成功。in jd');
+
+   // 触发 data_received 事件
    eventEmitter.emit('data_received11');
    eventEmitter.emit("william");
 }
 
 // 绑定 connection 事件处理程序
 eventEmitter.on('connection', connectHandler);
- 
+
 // 使用匿名函数绑定 data_received 事件
 eventEmitter.on('data_received11', function(){
    console.log('数据接收成功。');
@@ -23,8 +23,8 @@ eventEmitter.on('data_received11', function(){
 eventEmitter.on('william',function(){
 	console.log('自定义William事件触发成功');
 })
-console.log("程序执行完毕1。"); 
-// 触发 connection 事件 
+console.log("程序执行完毕1。");
+// 触发 connection 事件
 eventEmitter.emit('connection');
 
-console.log("程序执行完毕。"); 
+console.log("程序执行完毕。");
